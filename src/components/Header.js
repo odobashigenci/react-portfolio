2d4956c25e3e9626509564d7edc91295cb51c136
odobashigenci <121208981+odobashigenci@users.noreply.github.com>
2023-07-29 23:11:21 -0400

    styling home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+// Header.js
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
@@ -6,43 +7,61 @@ import Portfolio from './Portfolio';
 import Resume from './Resume';
 import Contact from './Contact';
 
-
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const toggleMenu = () => {
-        setMenuOpen(!isMenuOpen)
-    }
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <Router>
-        <div className="d-flex justify-content-center">
-     <nav className="navbar navbar-expand-lg navbar-light m-5 p-3 shadow">
-            <div className="container-fluid">
-                <a className="navbar-brand home" href="/">Home</a>
-                <button className="navbar-toggler" type="button"
-                    data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="{isMenuOpen}"
-                    onClick={toggleMenu}
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/portfolio">Portfolio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/resume">Resume</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact Me</a>
-                        </li>
-                    </ul>
-                </div>
+      <div className="d-flex justify-content-center">
+        <nav className="navbar navbar-expand-lg navbar-light m-5 p-3">
+          <div className="container-fluid">
+            <p className="navbar-brand text-white name">Genci Odobashi</p>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="{isMenuOpen}"
+              onClick={toggleMenu}
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}
+              id="navbarNav"
+            >
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/portfolio">
+                    Portfolio
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/resume">
+                    Resume
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
             </div>
+          </div>
         </nav>
-        </div>
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
