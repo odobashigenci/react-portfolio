71149fa38191feb7c4bd7b02258d5aab791b8fd4
odobashigenci <121208981+odobashigenci@users.noreply.github.com>
2023-08-27 05:35:04 -0400

    updating resume page animations

--- src/components/ImageRow.js
+++ src/components/ImageRow.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ImageCard from "./ImageCard";
 
-const ImageRow = ({ images, direction }) => {
+const ImageRow = ({ images }) => {
   return (
-    <div className={`d-flex justify-content-center space-between mx-5 my-5 row ${direction}`}>
+    <div className="imageRows d-flex justify-content-center space-between mx-5 my-5 row">
       {images.map((image, index) => (
         <ImageCard key={index} {...image} />
       ))}
     </div>
+
+    
   );
 };
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,14 +10,14 @@ import codeQuizPreview from "../assets/codeQuizPreview.png";
 
 function Portfolio() {
   const projects = [
-    // {
-    //   title: "CubCare",
-    //   imageSrc: cubCarePreview,
-    //   description:
-    //     "This is a childcare website where parents can meet babysitters for their children.",
-    //   githubLink: "https://github.com/tcunningham203/babysitter-finder",
-    //   liveLink: "https://cubcare-9dc4e13d9451.herokuapp.com/create-profile",
-    // },
+    {
+      title: "CubCare",
+      imageSrc: cubCarePreview,
+      description:
+        "This is a childcare website where parents can meet babysitters for their children.",
+      githubLink: "https://github.com/tcunningham203/babysitter-finder",
+      liveLink: "https://cubcare-9dc4e13d9451.herokuapp.com/create-profile",
+    },
     {
       title: "Break My Intake",
       imageSrc: breakMyIntakePreview,
